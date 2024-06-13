727be8b143d636d3b455f8617e01e4d3ff9ef01e
EngrArfin <arfin.cse.green.edu.bd@gmail.com@>
2024-06-13 15:40:13 +0600

    jwt update

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config()
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+/* 
+const res = require("express/lib/response");
+const { configDotenv } = require("dotenv"); */
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req, res, next) => {
+/* const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({ error: true, message: 'unauthorization access'});
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorization access" });
   }
   //bearer token
-  const token = authorization.split('') [1];
-  
+  const token = authorization.split("")[1];
+
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(401).send({ error: true, message: 'unauthorized access'})
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
     }
     req.decoded = decoded;
     next();
-  })
-}
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const res = require('express/lib/response');
-const { configDotenv } = require('dotenv');
+  });
+}; */
+
+const { MongoClient, ServerApiVersion, ObjectId, Admin } = require("mongodb");
 const uri = `mongodb+srv://${process.env.SK_User}:${process.env.SK_Pass}@cluster0.xu7sm0d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,7 +42,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -46,117 +52,152 @@ async function run() {
 
     const usersCollection = client.db("productSP").collection("users");
     const productColletion = client.db("productSP").collection("product");
-    const productCategoryColletion = client.db("productSP").collection("productCategory");
+    const productCategoryColletion = client
+      .db("productSP")
+      .collection("productCategory");
     const cabColletion = client.db("productSP").collection("cabs");
 
-
-    app.post('/jwt',(req, res) =>{
+    //jwt token
+    /* app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-      res.send({token})
-    })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    }); */
+
+    //verifyAdmin
+    /*  const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    }; */
 
     //users api
-    app.get('/users', async(req, res) =>{
-      const result = await usersCollection.find().toArray();
-      res.send(result);
-    });
+    app.get(
+      "/users",
+      /*  verifyJWT, verifyAdmin, */ async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      }
+    );
 
-    app.post('/users', async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email }
+      const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
 
-      if(existingUser){
-        return res.send({message: 'user already exist'})
+      if (existingUser) {
+        return res.send({ message: "user already exist" });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
-    app.patch('/users/admin/:id', async(req, res) => {
-      const id =req.params.id; 
-      const filter = {_id: new ObjectId(id) };
+    //admin
+    app.patch("/users/admin/:email", async (req, res) => {
+      const id = req.params.email;
+
+      if (req.decoded.email !== email) {
+        req.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: 'admin'
+          role: "admin",
         },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })
+    });
 
     /* Delete Users From database */
-    app.delete('/users/:id', async (req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
       res.send(result);
+    });
 
-    })
-
-    app.get('/product', async (req, res) => {
+    app.get("/product", async (req, res) => {
       const result = await productColletion.find().toArray();
-      res.send(result);
-    })
-
-    app.get('/productCategory', async (req, res) => {
-      const result = await productCategoryColletion.find().toArray();
-      res.send(result);
-    })
-
-    /* Cab Data Collection api for cab button in navBar */
-    app.get('/cabs', verifyJWT, async (req, res) => {
-      const email = req.query.email;
-      if (!email) {
-        res.send([]);
-      }
-
-      const decodedEmail = req.decoded.email;
-      if(!email){
-        return res.status(403).send({ error: true, message: 'forbided access'})
-      }
-
-      
-      const query = { email: email };
-      const result = await cabColletion.find(query).toArray();
       res.send(result);
     });
 
+    app.get("/productCategory", async (req, res) => {
+      const result = await productCategoryColletion.find().toArray();
+      res.send(result);
+    });
+
+    /* Cab Data Collection api for cab button in navBar */
+    app.get(
+      "/cabs",
+      /* verifyJWT, */ async (req, res) => {
+        const email = req.query.email;
+        if (!email) {
+          res.send([]);
+        }
+
+        /*  const decodedEmail = req.decoded.email;
+        if (!email) {
+          return res
+            .status(403)
+            .send({ error: true, message: "forbided access" });
+        }
+ */
+        const query = { email: email };
+        const result = await cabColletion.find(query).toArray();
+        res.send(result);
+      }
+    );
+
     /* Cab Data Collection For next page */
-    app.post('/cabs', async (req, res) => {
+    app.post("/cabs", async (req, res) => {
       const item = req.body;
       const result = await cabColletion.insertOne(item);
       res.send(result);
-    })
+    });
 
-    app.delete('/cabs/:id', async (req, res) => {
+    app.delete("/cabs/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cabColletion.deleteOne(query);
       res.send(result);
-
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
-
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Shop is sitting!')
-})
+app.get("/", (req, res) => {
+  res.send("Shop is sitting!");
+});
 
 app.listen(port, () => {
-  console.log(`Shop is setting ${port}`)
-})
-
+  console.log(`Shop is setting ${port}`);
+});
 
 /* 
 *------------------------
